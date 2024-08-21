c81e6240dbe2103c0cc5968f1270ee500da53ebf
kayla-pagan <114839282+kayla-pagan@users.noreply.github.com>
2024-08-21 18:13:30 -0400

    add google font links to head

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
+import { Link, NavLink } from 'react-router-dom'
 import './App.css'
 
 function App() {
-  
+
   return (
     <header>
       <h1>Discover <span>Taniti</span></h1>
