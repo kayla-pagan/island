b6e5c84963d91cfd5ca4fa0533a1b789069d5c03
kayla-pagan <114839282+kayla-pagan@users.noreply.github.com>
2024-08-22 14:52:26 -0400

    add style to header

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
 import React from 'react'
 import { Link, NavLink } from 'react-router-dom'
-import './App.css'
+// import './App.css'
 
 function App() {
 
   return (
     <header>
-      <h1>Discover <span>Taniti</span></h1>
+      <div class="header-container">
+        <h1>Discover <span>Taniti</span></h1>
 
-      <nav>
-        <a href="#">HOME</a>
-        <a href="">ABOUT</a>
-        <a href="">GETTING HERE</a>
-        <a href="">GETTING AROUND</a>
-        <a href="">HOTEL & RESORTS</a>
-        <a href="">RESTAURANTS</a>
-        <a href="">THINGS TO DO</a>
-      </nav>
+        <nav>
+          <a href="#">HOME</a>
+          <a href="">ABOUT</a>
+          <a href="">GETTING HERE</a>
+          <a href="">GETTING AROUND</a>
+          <a href="">HOTEL & RESORTS</a>
+          <a href="">RESTAURANTS</a>
+          <a href="">THINGS TO DO</a>
+        </nav>
+      </div>
     </header>
   )
 
