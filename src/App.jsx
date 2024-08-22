0b595f9a90cd661ddfb37c225ee64e6c5a2c26c0
kayla-pagan <114839282+kayla-pagan@users.noreply.github.com>
2024-08-22 15:00:21 -0400

    create component folder and Header component. Paste header into component

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,10 @@
 import React from 'react'
 import { Link, NavLink } from 'react-router-dom'
-// import './App.css'
-
+import Header from './components/header'
 function App() {
 
   return (
-    <header>
-      <div class="header-container">
-        <h1>Discover <span>Taniti</span></h1>
-
-        <nav>
-          <a href="#">HOME</a>
-          <a href="">ABOUT</a>
-          <a href="">GETTING HERE</a>
-          <a href="">GETTING AROUND</a>
-          <a href="">HOTEL & RESORTS</a>
-          <a href="">RESTAURANTS</a>
-          <a href="">THINGS TO DO</a>
-        </nav>
-      </div>
-    </header>
+    <Header />
   )
 
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+
+export default function Header(){
+
+    return (
+        <header>
+            <div class="header-container">
+                <h1>Discover <span>Taniti</span></h1>
+
+                <nav>
+                <a href="#">HOME</a>
+                <a href="">ABOUT</a>
+                <a href="">GETTING HERE</a>
+                <a href="">GETTING AROUND</a>
+                <a href="">HOTEL & RESORTS</a>
+                <a href="">RESTAURANTS</a>
+                <a href="">THINGS TO DO</a>
+                </nav>
+            </div>
+        </header>
+    )
+}
