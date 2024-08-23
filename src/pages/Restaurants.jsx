38e494501fa61301b22fcf91019bc95cbe996532
kayla-pagan <114839282+kayla-pagan@users.noreply.github.com>
2024-08-23 15:43:57 -0400

    change about pg imgs. Add imgs to restaurants pg

--- src/pages/Restaurants.jsx
+++ src/pages/Restaurants.jsx
@@ -11,30 +11,37 @@ export default function Restaurants(){
                     tastes.
                 </p>
 
-                <div>
-                    <h3>Local</h3>
-                    <p>
-                        These restaurants are for those looking to explore with 
-                        their tastebuds and experience the islands local dishes. 
-                        We offer a variety of seafood and rice platters that give 
-                        any visitor a true taste of Taniti culture. These meals 
-                        celebrate our heritage and include recipes that have been 
-                        passed down from generation to generation.
-                    </p>
+                <div className="restaurants--container">
+                    <div className="restaurants--info">
+                        <h3>Local</h3>
+                        <p>
+                            These restaurants are for those looking to explore with 
+                            their tastebuds and experience the islands local dishes. 
+                            We offer a variety of seafood and rice platters that give 
+                            any visitor a true taste of Taniti culture. These meals 
+                            celebrate our heritage and include recipes that have been 
+                            passed down from generation to generation.
+                        </p>
+                    </div>
+                    <div className="restaurants--seafood"></div>
                 </div>
 
-                <div>
-                    <h3>American</h3>
-                    <p>
-                        Many of our visitors come from America, so some restuarants 
-                        decided to offer a handful of American style dishes. If you 
-                        prefer to enjoy comfort food at it's finest, these options 
-                        might be best for you. All dishes are prepared with a twist 
-                        as we feel that it wouldn't be complete with some Taniti flavor.
-                    </p>
+                <div className="restaurants--container">
+                    <div className="restaurants--info">
+                        <h3>American</h3>
+                        <p>
+                            Many of our visitors come from America, so some restuarants 
+                            decided to offer a handful of American style dishes. If you 
+                            prefer to enjoy comfort food at it's finest, these options 
+                            might be best for you. All dishes are prepared with a twist 
+                            as we feel that it wouldn't be complete with some Taniti flavor.
+                        </p>
+                    </div>
+                    <div className="restaurants--burger"></div>
                 </div>
 
-                <div>
+                <div className="restaurants--container">
+                    <div className="restaurants--info">
                     <h3>Pan-Asian</h3>
                     <p>
                         For those looking for the taste of Asian cuisine, look no further.
@@ -43,6 +50,8 @@ export default function Restaurants(){
                         like sushi, kimchi, and curry but with some additional hints of 
                         Taniti in between each bite.
                     </p>
+                    </div>
+                    <div className="restaurants--sushi"></div>
                 </div>
             </div>
         </main>
