b05368e2ebed84248eb82f6c094fb9060e258b9f
kayla-pagan <114839282+kayla-pagan@users.noreply.github.com>
2024-08-24 01:07:28 -0400

    update header import to layout pg

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import Header from "./header";
+import Header from "../components/Header";
 
 export default function Layout(){
     return (
