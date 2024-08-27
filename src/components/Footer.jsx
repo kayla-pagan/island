47f5dfd4784a1425df070f5496d4895fe992409a
kayla-pagan <114839282+kayla-pagan@users.noreply.github.com>
2024-08-26 22:57:56 -0400

    create footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer(){
+    return (
+        <p>
+            Copyright © 2024. All rights reserved. Discover Taniti® is a registered trademark.
+        </p>
+    )
+}
